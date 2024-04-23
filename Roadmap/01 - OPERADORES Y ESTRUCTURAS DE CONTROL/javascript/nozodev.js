bbef7846cd607caeb105261496a5c12870fb25e9
NozoDev <kevinroj326@gmail.com>
2024-04-23 01:06:58 -0500

    haciendo los ejercicios de repaso del lenguaje

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/nozodev.js
@@ -0,0 +1,43 @@
+/*
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */
+
+//? Operadores Aritmeticos..
+
+// Operadores aritmeticos toma valores numericos (ya sean literales o variables) como sus iperandos y devuelve un solo valor numerico.
+
+console.log(5 + 4); // suma
+console.log(10 - 5); // resta
+console.log(4 * 8); // multiplicacion
+console.log(2 / 4); // division
+
+//! Operadores Logicos
+
+// Los Operadores logicos se utilizan normalmente con valores booleanos(logicos): cuando lo son devuelven un valoor booleano
+
+//Este operador AND && , devuelve true si ambos operandos son true , de lo contraio de vuelve false.
+
+console.log(true && true);
+console.log(false && true);
+console.log(false && false);
+console.log(true && false);
+
+// Operador OR ||, devuelve true si alguno de los operando es true, si ambos son falsos , devuelve false
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
